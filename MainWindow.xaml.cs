39ca33666e5fe760d014652f3b1b9688239bc4fb
joshc0des <joshuahooper3@gmail.com>
2023-02-16 10:38:12 -0600

    completed project

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace JSON_P_GameOfThronesQuote
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<Quote> _Quotes = new List<Quote>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void genQuoteBTN_Click(object sender, RoutedEventArgs e)
+        {
+            txtBoxQuote.Clear();
+
+            using (var client = new HttpClient())
+            {
+                string json = client.GetStringAsync("https://api.gameofthronesquotes.xyz/v1/random").Result;
+
+                Quote newQuote = JsonConvert.DeserializeObject<Quote>(json);
+
+                txtBoxQuote.Text = newQuote.ToString();
+                listBoxData.Items.Add(newQuote);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string jsonOut = JsonConvert.SerializeObject(listBoxData.Items);
+
+            File.WriteAllText($"C:\\Users\\joshu\\Downloads\\Games_Export.json", jsonOut);
+
+            MessageBox.Show("Successfully saved export to downloads.");
+        }
+    }
+}
---
+++ Quote.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSON_P_GameOfThronesQuote
+{
+    public class Quote
+    {
+        public string sentence { get; set; }
+        public character character { get; set; }
+
+        public override string ToString()
+        {
+            return $"\"{sentence}\" - {character.name}";
+        }
+    }
+
+    public class character
+    {
+        public string name { get; set; }
+        public string slug { get; set; }
+        public house house { get; set; }
+    }
+
+    public class house
+    {
+        public string name { get; set; }
+        public string slug { get; set; }
+    }
+}
